8caf959fd7eb37c0a636e14d5f3d847f2c52e58b
Florian Pigorsch <fpigorsch@gmail.com>
2020-10-13 08:38:41 +0200

    gradients

--- main.js
+++ main.js
@@ -1,27 +1,17 @@
 function main() {
-    switch_color();
+	switch_color();
     const switcher = document.getElementById("hero");
     switcher.onclick = function () {
         switch_color();
     };
+	
 }
 
 function switch_color() {
-    const colors = [
-        "is-info",
-        "is-primary",
-        "is-danger",
-        "is-link",
-        "is-success",
-        "is-warning",
-        "is-dark",
-        "is-light"
-    ];
+	const colors = ["color1", "color2", "color3", "color4"];
 
-    const hero = document.getElementById("hero");
-    
-    var current_color = "";
-    hero.classList.forEach((cls) => {
+	var current_color = "";
+    document.body.classList.forEach((cls) => {
         if (colors.includes(cls)) {
             current_color = cls;
         }
@@ -32,31 +22,6 @@ function switch_color() {
         random_color = colors[Math.floor(Math.random() * colors.length)];
     }
 
-    hero.classList.add(random_color);
-    hero.classList.remove(current_color);
-
-    document.head.querySelector('meta[name="theme-color"]').content = color_to_hex(random_color);
-}
-
-function color_to_hex(color) {
-    switch (color) {
-        case "is-info":
-            return "#3298dc";
-        case "is-primary":
-            return "#00d1b2";
-        case "is-danger":
-            return "#f14668";
-        case "is-link":
-            return "#3273dc";
-        case "is-success":
-            return "#48c774";
-        case "is-warning":
-            return "#ffdd57";
-        case "is-dark":
-            return "#363636";
-        case "is-light":
-            return "#f5f5f5";
-    }
-
-    return "#48c774";
+    document.body.classList.add(random_color);
+    document.body.classList.remove(current_color);
 }
